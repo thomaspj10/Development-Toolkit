6732c11cdb021fe1460fd31e1ca620949ad33bf8
Thomas P <thomaspj2003@gmail.com>
2023-05-25 18:00:47 +0200

    Try to fix the stub not found error.

--- setup.py
+++ setup.py
@@ -1,7 +1,4 @@
 from setuptools import setup
 
-with open('requirements.txt') as f:
-    install_requirements = f.read().splitlines()
-
 if __name__ == "__main__":
-    setup(requires=install_requirements)
+    setup()
