9abd72d700b6c10cceff24ecf6ec96c65731e051
Thomas P <thomaspj2003@gmail.com>
2023-05-26 19:36:54 +0200

    Create unit tests for querying.

--- tests/unit/models.py
+++ tests/unit/models.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from dataclasses import dataclass, field
 from devkit.sql import Model, fetch_as
+from devkit.sql.querying import ColumnDefinition, TableDefinition
 
 @dataclass(slots=True)
 class Address(Model):
@@ -10,7 +11,7 @@ class Address(Model):
 
     @staticmethod
     def find_by_id(id: int) -> Address | None:
-        result = fetch_as(f"select rowid as id, * from `Address` where `rowid` = {id}", Address)
+        result = fetch_as(f"select rowid as id, * from `Address` where `rowid` = ?", Address, [id])
         
         if len(result) == 0:
             return None
@@ -21,4 +22,10 @@ class Address(Model):
     def find_all() -> list[Address]:
         return fetch_as(f"select rowid as id, * from `Address`", Address)
 
+
+class AddressTable(TableDefinition[Address, 'AddressTable']):
+    ID: ColumnDefinition[AddressTable, int] = ColumnDefinition('id')
+    NAME: ColumnDefinition[AddressTable, str] = ColumnDefinition('name')
+
+ADDRESS = AddressTable('Address', Address)
 
--- tests/unit/test_query.py
+++ tests/unit/test_query.py
@@ -16,6 +16,7 @@ class TestSimpleModelInteractions(unittest.TestCase):
         sql.set_sqlite_file(DB_FILE)
         sql.execute("drop table if exists Address")
         sql.execute("create table Address (id integer primary key autoincrement, name varchar(255))")
+    
     @classmethod
     def tearDownClass(cls):
         sql.close_connection()
@@ -24,12 +25,62 @@ class TestSimpleModelInteractions(unittest.TestCase):
     def test_01_create_data(self):
         address_with_name = models.Address("Random name")
         address_without_name = models.Address(None)
+        address_with_name_2 = models.Address("Another random name")
         
         self.assertEqual(address_with_name.id, 1)
         self.assertEqual(address_with_name.name, "Random name")
         
         self.assertEqual(address_without_name.id, 2)
         self.assertEqual(address_without_name.name, None)
+
+        self.assertEqual(address_with_name_2.id, 3)
+        self.assertEqual(address_with_name_2.name, "Another random name")
+
+    def test_02_select(self):
+        addresses = sql.select_from(models.ADDRESS).fetch()
+
+        self.assertEqual(len(addresses), 3)
+        self.assertEqual(addresses[0].id, 1)
+        self.assertEqual(addresses[1].id, 2)
+        self.assertEqual(addresses[2].id, 3)
+
+    def test_03_select_conditional(self):
+        addresses = sql.select_from(models.ADDRESS).where(models.ADDRESS.ID.eq(1)).fetch()
+
+        self.assertEqual(len(addresses), 1)
+        self.assertEqual(addresses[0].id, 1)
+
+        address = sql.select_from(models.ADDRESS).where(models.ADDRESS.ID.eq(1)).fetch_one()
+        self.assertNotEqual(address, None)
+
+        if address != None:
+            self.assertEqual(address.id, 1)
+
+    def test_04_select_conditional_extra(self):
+        addresses_1 = sql.select_from(models.ADDRESS).where(models.ADDRESS.ID.in_([1, 2])).fetch()
+        self.assertEqual(len(addresses_1), 2)
+
+        addresses_2 = sql.select_from(models.ADDRESS).where(models.ADDRESS.ID.gt(2)).fetch()
+        self.assertEqual(len(addresses_2), 1)
+
+        addresses_3 = sql.select_from(models.ADDRESS).where(models.ADDRESS.ID.lt(2)).fetch()
+        self.assertEqual(len(addresses_3), 1)
+
+        addresses_4 = sql.select_from(models.ADDRESS).where(models.ADDRESS.ID.ge(2)).fetch()
+        self.assertEqual(len(addresses_4), 2)
+
+        addresses_5 = sql.select_from(models.ADDRESS).where(models.ADDRESS.ID.le(2)).fetch()
+        self.assertEqual(len(addresses_5), 2)
+
+    def test_05_select_limit(self):
+        addresses_1 = sql.select_from(models.ADDRESS).limit(1).fetch()
+        self.assertEqual(len(addresses_1), 1)
+
+        addresses_2 = sql.select_from(models.ADDRESS).limit(2).fetch()
+        self.assertEqual(len(addresses_2), 2)
+
+        addresses_3 = sql.select_from(models.ADDRESS).limit(3).fetch()
+        self.assertEqual(len(addresses_3), 3)
 
 if __name__ == '__main__':
     unittest.main()
